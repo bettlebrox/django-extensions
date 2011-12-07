aba7ad37efdb5fdcaa014204c988303197faf8eb
David Burke <david.burke@oceanfree.net>
2011-12-07 11:20:45 +0000

    fix for #81 - dont migrate is south installed

--- django_extensions/tests/encrypted_fields.py
+++ django_extensions/tests/encrypted_fields.py
@@ -28,7 +28,11 @@ class EncryptedFieldsTestCase(unittest.TestCase):
         self.old_installed_apps = settings.INSTALLED_APPS
         settings.INSTALLED_APPS.append('django_extensions.tests')
         loading.cache.loaded = False
-        call_command('syncdb', verbosity=0)
+        migrate = True
+        for app in settings.INSTALLED_APPS:
+            if app == "south":
+                migrate = False
+        call_command('syncdb', verbosity=0, migrate=migrate)
 
     def tearDown(self):
         settings.INSTALLED_APPS = self.old_installed_apps
