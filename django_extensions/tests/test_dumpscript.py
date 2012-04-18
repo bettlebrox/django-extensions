0fd17de29ea5065232762e1377085f8aac75fe61
chrisma <c.matthies.mail@gmail.com>
2012-04-18 13:55:18 +0300

    Fix issue #155 : Redirect stderr in test_dumpscript.py to avoid polluting test output.

--- django_extensions/tests/test_dumpscript.py
+++ django_extensions/tests/test_dumpscript.py
@@ -10,9 +10,12 @@ class DumpScriptTests(FieldTestCase):
         super(DumpScriptTests, self).setUp()
         self.real_stdout = sys.stdout
         sys.stdout = StringIO()
+        self.real_stderr = sys.stderr
+        sys.stderr = StringIO()
 
     def tearDown(self):
         sys.stdout = self.real_stdout
+        sys.stderr = self.real_stderr
 
     def test_runs(self):
         # lame test...does it run?
